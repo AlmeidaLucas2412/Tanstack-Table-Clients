18945d2a0e4c8fc774174534eafeed66be78ed5e
Lucas <lucasprepara12@gmail.com>
2025-05-22 14:03:14 -0300

    Minor style improvements

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -2,7 +2,7 @@ import { ThemeToggler } from "../theme-toggler";
 
 export const Header = () => {
   return (
-    <header className="fixed top-0 z-50 w-full shadow-md shadow-foreground/20">
+    <header className="w-full shadow-md shadow-foreground/20">
       <div className="flex justify-between items-center p-6">
         <p className="text-2xl font-light">
           Tanstack<span className="font-semibold">Table</span>
