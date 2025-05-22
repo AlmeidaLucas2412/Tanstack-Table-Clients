a227e6e777fcbf4e3da713041080bc237dff1fb6
Lucas <lucasprepara12@gmail.com>
2025-05-22 10:07:22 -0300

    Creates the project and install first dependencies and components

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Header } from "@/components/layout/header";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <main className="flex min-h-screen flex-col items-center justify-center">
+        <h1>Home</h1>
+      </main>
+    </>
+  );
+}
